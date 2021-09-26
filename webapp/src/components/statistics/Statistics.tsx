9be8257494a4a99119f39e03af845177860b2416
Brahim Kellou <brahimkellou@outlook.com>
2021-09-26 07:03:48 +0200

    fix: reformat code

--- webapp/src/components/statistics/Statistics.tsx
+++ webapp/src/components/statistics/Statistics.tsx
@@ -26,18 +26,14 @@ export default function Statistics() {
       });
   }, []);
 
-  const initHeatMapData = () => {
-    heatmapInstanceRef.current.setData({
-      max: 5,
-      data: points
-    });
-  }
-
   React.useEffect(() => {
     if (points.length) {
-      initHeatMapData()
+      heatmapInstanceRef.current.setData({
+        max: 5,
+        data: points
+      });
     }
-  }, [points, initHeatMapData])
+  }, [points])
 
   const data = {
     labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
